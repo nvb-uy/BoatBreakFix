245cde0a05de50208a2bcc3ba4514a3c3ea4672e
nvb-uy <almhak@proton.me>
2022-11-22 10:10:44 -0600

    -+ Fix crash on 1.19.1

--- FABRIC/src/main/java/elocindev/boatbreakfix/fabric_quilt/mixin/BoatEntityMixin.java
+++ FABRIC/src/main/java/elocindev/boatbreakfix/fabric_quilt/mixin/BoatEntityMixin.java
@@ -12,7 +12,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(BoatEntity.class)
 public class BoatEntityMixin {
 	
-	@Inject(at = @At("HEAD"), method = "fall()V", cancellable = true)
+	@Inject(at = @At("HEAD"), method = "fall", cancellable = true)
 	private void fall(double heightDifference, boolean onGround, BlockState state, BlockPos landedPosition, CallbackInfo info) {
 		if (onGround)
 			info.cancel();
